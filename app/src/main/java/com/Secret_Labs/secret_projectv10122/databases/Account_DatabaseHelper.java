cbb747b367207adda1e97b2d9958aa84644c281f
devsda12 <jmbalster@hotmail.com>
2019-03-02 16:03:55 +0100

    Database second table to hold conversations added

--- app/src/main/java/com/Secret_Labs/secret_projectv10122/databases/Account_DatabaseHelper.java
+++ app/src/main/java/com/Secret_Labs/secret_projectv10122/databases/Account_DatabaseHelper.java
@@ -7,33 +7,46 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
 import com.Secret_Labs.secret_projectv10122.models.Obj_AccountInfo;
+import com.android.volley.toolbox.StringRequest;
 
 public class Account_DatabaseHelper extends SQLiteOpenHelper {
 
 
     //Default constructor
     public Account_DatabaseHelper(@Nullable Context context) {
-        super(context, DatabaseInfo.Sapp_Acc_DB.DBNAME, null, DatabaseInfo.Sapp_Acc_DB.DBVERSION);
+        super(context, DatabaseInfo.DBNAME, null, DatabaseInfo.DBVERSION);
     }
 
     //Oncreate will be called when there will be an object created of the Account_DatabaseHelper. When the database already exists this code will not be executed
     @Override
     public void onCreate(SQLiteDatabase db){
-        String createTable = "CREATE TABLE " + DatabaseInfo.Sapp_Acc_DB.TABLE_NAME + "("
-                + DatabaseInfo.Sapp_Acc_DB.ACC_ID_COLUMN + " INTEGER PRIMARY KEY,"
-                + DatabaseInfo.Sapp_Acc_DB.ACC_USERNAME_COLUMN + " TEXT,"
-                + DatabaseInfo.Sapp_Acc_DB.ACC_PASSWORD_COLUMN + " TEXT,"
-                + DatabaseInfo.Sapp_Acc_DB.ACC_REMEMBERLOGIN_COLUMN + " INTEGER,"
-                + DatabaseInfo.Sapp_Acc_DB.ACC_LASTLOGIN_COLUMN + " TEXT" + ")";
+        String createAccTable = "CREATE TABLE " + DatabaseInfo.Sapp_Table_Acc.ACC_TABLE_NAME + "("
+                + DatabaseInfo.Sapp_Table_Acc.ACC_ID_COLUMN + " INTEGER PRIMARY KEY,"
+                + DatabaseInfo.Sapp_Table_Acc.ACC_USERNAME_COLUMN + " TEXT,"
+                + DatabaseInfo.Sapp_Table_Acc.ACC_PASSWORD_COLUMN + " TEXT,"
+                + DatabaseInfo.Sapp_Table_Acc.ACC_REMEMBERLOGIN_COLUMN + " INTEGER,"
+                + DatabaseInfo.Sapp_Table_Acc.ACC_LASTLOGIN_COLUMN + " TEXT" + ")";
 
-        db.execSQL(createTable);
+        db.execSQL(createAccTable);
+
+        String createConvTable = "CREATE TABLE " + DatabaseInfo.Sapp_Table_Conv.CONV_TABLE_NAME + "("
+                + DatabaseInfo.Sapp_Table_Conv.CONV_ID_COLUMN + " INTEGER PRIMARY KEY,"
+                + DatabaseInfo.Sapp_Table_Conv.CONV_ACC_ID_COLUMN + " INTEGER,"
+                + DatabaseInfo.Sapp_Table_Conv.CONV_PARTNER_ID_COLUMN + " TEXT,"
+                + DatabaseInfo.Sapp_Table_Conv.CONV_PARTNER_USERNAME_COLUMN + " TEXT,"
+                + DatabaseInfo.Sapp_Table_Conv.CONV_LAST_MESSAGE_COLUMN + " TEXT,"
+                + DatabaseInfo.Sapp_Table_Conv.CONV_LAST_MESSAGE_DATE_COLUMN + " TEXT,"
+                + "FOREIGN KEY(" + DatabaseInfo.Sapp_Table_Conv.CONV_ACC_ID_COLUMN + ") REFERENCES " + DatabaseInfo.Sapp_Table_Acc.ACC_TABLE_NAME + "(" + DatabaseInfo.Sapp_Table_Acc.ACC_ID_COLUMN + ")" +  ")";
+
+        db.execSQL(createConvTable);
     }
 
     //Onupgrade will be called when the database version changes and so all the tables will be dropped and a new table will be created
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         //Drop if table exists
-        db.execSQL("DROP TABLE IF EXISTS " + DatabaseInfo.Sapp_Acc_DB.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseInfo.Sapp_Table_Conv.CONV_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseInfo.Sapp_Table_Acc.ACC_TABLE_NAME);
         onCreate(db);
     }
 
@@ -45,14 +58,14 @@ public class Account_DatabaseHelper extends SQLiteOpenHelper {
         //Create content values object and add the values into it
         ContentValues addableValues = new ContentValues();
 
-        addableValues.put(DatabaseInfo.Sapp_Acc_DB.ACC_ID_COLUMN, accountInfo.getAcc_Id());
-        addableValues.put(DatabaseInfo.Sapp_Acc_DB.ACC_USERNAME_COLUMN, accountInfo.getAcc_Username());
-        addableValues.put(DatabaseInfo.Sapp_Acc_DB.ACC_PASSWORD_COLUMN, accountInfo.getAcc_Password());
-        addableValues.put(DatabaseInfo.Sapp_Acc_DB.ACC_REMEMBERLOGIN_COLUMN, accountInfo.getAcc_RememberLogin());
-        addableValues.put(DatabaseInfo.Sapp_Acc_DB.ACC_LASTLOGIN_COLUMN, accountInfo.getAcc_Last_Login());
+        addableValues.put(DatabaseInfo.Sapp_Table_Acc.ACC_ID_COLUMN, accountInfo.getAcc_Id());
+        addableValues.put(DatabaseInfo.Sapp_Table_Acc.ACC_USERNAME_COLUMN, accountInfo.getAcc_Username());
+        addableValues.put(DatabaseInfo.Sapp_Table_Acc.ACC_PASSWORD_COLUMN, accountInfo.getAcc_Password());
+        addableValues.put(DatabaseInfo.Sapp_Table_Acc.ACC_REMEMBERLOGIN_COLUMN, accountInfo.getAcc_RememberLogin());
+        addableValues.put(DatabaseInfo.Sapp_Table_Acc.ACC_LASTLOGIN_COLUMN, accountInfo.getAcc_Last_Login());
 
         //Inserting data into database and getting result code back
-        long result = db.insert(DatabaseInfo.Sapp_Acc_DB.TABLE_NAME, null, addableValues);
+        long result = db.insert(DatabaseInfo.Sapp_Table_Acc.ACC_TABLE_NAME, null, addableValues);
 
         //Returning if the data was inserted succesfully for the activity to handle further
         if(result == -1){
--- app/src/main/java/com/Secret_Labs/secret_projectv10122/databases/DatabaseInfo.java
+++ app/src/main/java/com/Secret_Labs/secret_projectv10122/databases/DatabaseInfo.java
@@ -2,18 +2,35 @@ package com.Secret_Labs.secret_projectv10122.databases;
 
 public class DatabaseInfo {
 
-    public class Sapp_Acc_DB{
-        //Main variables of the database
-        public static final String DBNAME = "Sapp_Acc_DB";
-        public static final String TABLE_NAME = "Acc_Table";
-        public static final int DBVERSION = 1;
+    //Main variables of the database
+    public static final String DBNAME = "Sapp_Acc_DB";
+    public static final int DBVERSION = 1;
 
-        //Column names of the database
+    //Main table properties for stored accounts on the device
+    public class Sapp_Table_Acc{
+        //Main variables of the table
+        public static final String ACC_TABLE_NAME = "Acc_Table";
+
+        //Column names of the table
         public static final String ACC_ID_COLUMN = "acc_Id";
         public static final String ACC_USERNAME_COLUMN = "acc_Username";
         public static final String ACC_PASSWORD_COLUMN = "acc_Password";
         public static final String ACC_REMEMBERLOGIN_COLUMN = "acc_Rememberlogin";
         public static final String ACC_LASTLOGIN_COLUMN = "acc_Lastlogin";
+    }
+
+    //Secondary table properties for the conversation overview table
+    public class Sapp_Table_Conv{
+        //Main variables for the table
+        public static final String CONV_TABLE_NAME = "Conv_Table";
+
+        //Column names for the table
+        public static final String CONV_ID_COLUMN = "conv_Id";
+        public static final String CONV_ACC_ID_COLUMN = "conv_Acc_Id";
+        public static final String CONV_PARTNER_ID_COLUMN = "conv_Partner_Id";
+        public static final String CONV_PARTNER_USERNAME_COLUMN = "conv_Partner_Username";
+        public static final String CONV_LAST_MESSAGE_COLUMN = "conv_Last_Message";
+        public static final String CONV_LAST_MESSAGE_DATE_COLUMN = "conv_Last_Message_Date";
     }
 
 }
