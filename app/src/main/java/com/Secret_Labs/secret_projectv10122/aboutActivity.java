e06de6f81c952dc83fd7bb01ae1599c443242cc2
remon <remonhoogendijk@cs,com>
2019-04-09 14:44:50 +0200

    close to final changes statistics

---
+++ app/src/main/java/com/Secret_Labs/secret_projectv10122/aboutActivity.java
@@ -0,0 +1,13 @@
+package com.Secret_Labs.secret_projectv10122;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
